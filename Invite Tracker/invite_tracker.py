9002bb6f666af17ae7ec1fe634183c836d9dc232
stroupbslayen <strouprd@gmail.com>
2017-12-19 08:03:19 -0800

    A list of Discord bots

--- Invite Tracker/invite_tracker.py
+++ Invite Tracker/invite_tracker.py
@@ -1,5 +1,4 @@
-#A bot that lists total uses a members invite link has been used. Really only useful for links that don't expire, for now.
-import discord,datetime
+import discord
 client=discord.Client()
 
 @client.event
@@ -15,17 +14,12 @@ async def on_message(message):
 		embed=discord.Embed(title='__Invites from {}__'.format(message.author.name))
 		invites = await client.invites_from(message.server)
 		for invite in invites:
-			if invite.inviter == message.author:
+			if invite.inviter == message.author and invite.max_age==0:
 				total_uses += invite.uses
 				embed.add_field(name='Invite',value=invite.id)
 				embed.add_field(name='Uses',value=invite.uses)
-				if invite.max_age == 0:
-					expires='Never'
-				else:
-					expires=datetime.timedelta(seconds=invite.max_age)
-				embed.add_field(name='Expires',value=expires)
+				embed.add_field(name='Expires',value='Never')
 		embed.add_field(name='__Total Uses__',value=total_uses)
 		await client.send_message(message.channel,embed=embed)
-
-
+		
 client.run('TOKEN')
