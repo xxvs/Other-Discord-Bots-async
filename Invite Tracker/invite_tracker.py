6a4dc983ba5a59a4b3bed992986ebb2b6a1ab312
stroupbslayen <strouprd@gmail.com>
2017-12-21 05:56:06 -0800

    Added bot game status

--- Invite Tracker/invite_tracker.py
+++ Invite Tracker/invite_tracker.py
@@ -17,6 +17,7 @@ async def on_ready():
 	global role_list
 	print('Logged in as: '+client.user.name)
 	print('Bot ID: '+client.user.id)
+	await client.change_presence(game=discord.Game(name='!invites - list your invites'))
 	print('------\n')
 	for server in client.servers:
 		role_list=dict((role.name,role) for role in server.roles)
