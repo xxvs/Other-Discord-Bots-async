a6bf463f8fa656228f91f9d3e12d3a7004bc0583
stroupbslayen <strouprd@gmail.com>
2017-12-31 15:46:34 -0800

    Simple Currency Bot

---
+++ Currency/currency_bot.py
@@ -0,0 +1,71 @@
+'''
+A simple currency bot.
+Add roles that are allowed to give currency to others in the server. Use !give @member {number} to added
+Members can check their balance with !bank
+'''
+
+import discord
+from discord.ext import commands
+import os,json
+
+#Add the roles that are able to give currency to people
+approved_roles=['Admin','Mod']
+
+def read_json(file_name):
+	if file_name.endswith('.json')==False:
+		file_name=file_name+'.json'
+	if not os.path.isfile(file_name):
+		list_name=open(file_name,"w+")
+		list_name={}
+	else:
+		try:
+			with open(file_name) as f:
+				list_name = json.load(f)
+		except ValueError:
+			list_name={}
+	return list_name
+
+def edit_json(file_name,items):
+	if file_name.endswith('.json')==False:
+		file_name=file_name+'.json'
+	with open(file_name,"w") as f:
+		f.write(json.dumps(items))
+
+currency=read_json('currency')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+	print('Logged in as: {}'.format(bot.user))
+	print('User ID: {}'.format(bot.user.id))
+
+@bot.command(pass_context=True)
+async def give(ctx,member: discord.Member=None, amount: int=None):
+	'''Example: !give @member 100'''
+	if amount==None or int(amount)==False:
+		await bot.say('Incorrect format.')
+		return
+
+	give=False
+	author=ctx.message.author
+	for role in author.roles:
+		if role.name in approved_roles:
+			give=True
+			break
+	if give==True:
+		if member.id not in currency:
+			currency[member.id]=0
+		currency[member.id]+=amount
+		edit_json('currency',currency)
+		await bot.say("{} has been added to {}'s account".format(amount,member.mention))
+	else:
+		await bot.say('You do not have permissios to add funds.')
+
+@bot.command(pass_context=True)
+async def bank(ctx):
+	account=ctx.message.author
+	await bot.say('{} : **{}**'.format(account.mention,currency[account.id]))
+
+
+bot.run('TOKEN')
