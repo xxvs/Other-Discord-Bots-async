231aae0523933dbfb5728096ec9859556c0dc808
stroupbslayen <strouprd@gmail.com>
2018-03-21 19:14:56 -0700

    Create trigger-response.py

---
+++ trigger-response/trigger-response.py
@@ -0,0 +1,25 @@
+# A bot that will respond to certain words in messages sent
+
+import discord,random
+from discord.ext import commands
+
+bot=commands.Bot(command_prefix='!')
+
+trigger_words=['edit','this','list'] #add the misspelled words to the list
+responses=['Yum!','LIES!','more things!','edit'] #add more responses for the bot
+
+@bot.event
+async def on_ready():
+	global timeout_role,timeout_channel
+	print(bot.user.name)
+	print(bot.user.id)
+
+@bot.event
+async def on_message(message):
+	if message.author == bot.user:
+		return
+	if any(word.lower() in message.content.lower() for word in trigger_words):
+		response=random.choice(responses)
+		await bot.send_message(message.channel,response)
+
+bot.run('TOKEN')
