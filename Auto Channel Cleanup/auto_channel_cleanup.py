503276f2d8f7cf6f24f844b1662567665537d9b7
stroupbslayen <strouprd@gmail.com>
2018-04-26 09:52:31 -0700

    Update auto_channel_cleanup.py

--- Auto Channel Cleanup/auto_channel_cleanup.py
+++ Auto Channel Cleanup/auto_channel_cleanup.py
@@ -1,27 +1,37 @@
-#DELETES POSTS FROM THE SPECIFIED CHANNEL IF THEY ARE MORE THAN A DAY OLD
+#DELETES POSTS FROM THE SPECIFIED CHANNELS IF THEY ARE MORE THAN A DAY OLD
 import discord,asyncio
 import datetime
-client=discord.Client()
 
-@client.event
+bot=discord.Client()
+
+channel_list=['ID# OF CHANNEL 1','ID# OF CHANNEL 2'] #list of channel ID#'s to remove messages from
+message_age=1 #time, in days, messages need to be to be deleted
+clean_time=3600 #time, in seconds, to check for old messages
+
+@bot.event
 async def on_ready():
-	print('Logged in as: '+client.user.name)
-	print('Bot ID: '+client.user.id)
+	print('Logged in as: '+bot.user.name)
+	print('Bot ID: '+bot.user.id)
 	print('------')
 
-
 async def channel_cleanup():
-	await client.wait_until_ready()
-	for server in client.servers:
-		channel_list=dict( (channel.name,channel) for channel in server.channels)
-	mlist=[]
-	async for message in client.logs_from(channel_list['YOUR CHANNEL NAME HERE']):
-		tdiff=(datetime.datetime.now()-message.timestamp).days
-		if tdiff>=1: #Age, in days, that posts have to be to get deleted
-			mlist.append(message)
-	if len(mlist)>0:
-		await client.delete_messages(mlist)
-	await asyncio.sleep(60) #Time, in seconds, between checks for old posts
+	await bot.wait_until_ready()
+	while True:
+		for server in bot.servers:
+			for channel_id in channel_list:
+				channel = discord.utils.get(server.channels,id=channel_id)
+				if channel is not None:
+					print('Checking channel: '+channel.name)
+					mlist=[]
+					async for message in bot.logs_from(channel):
+						tdiff=(datetime.datetime.now()-message.timestamp).days
+						if tdiff>=14:
+							await bot.delete_message(message)
+						elif tdiff>=message_age:
+							mlist.append(message)
+					if len(mlist)>0:
+						await bot.delete_messages(mlist)
+		await asyncio.sleep(clean_time)
 
-client.loop.create_task(channel_cleanup())
-client.run('TOKEN')
+bot.loop.create_task(channel_cleanup())
+bot.run('TOKEN')
