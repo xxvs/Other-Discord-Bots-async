351721dab5486578099ca54b26444226305013fd
StroupBSlayen <strouprd@gmail.com>
2018-05-09 08:36:21 -0700

    Update to queue_bot

--- queue-bot/queue_bot.py
+++ queue-bot/queue_bot.py
@@ -1,78 +1,114 @@
+# queue bot
+
 import discord
 from discord.ext import commands
 
-bot=commands.Bot(command_prefix='!')
+prefix = '!'  # change this to whatever prefix you'd like
 
-approved_roles=['Admin','Bot'] #replace with the name of the roles that can use call and clear commands
+bot = commands.Bot(command_prefix=prefix)
 
-_queue=[]
+# add roles that can use some commands
+approved_roles = ['Admin', 'Bot', 'Mod']
+
 
 def is_approved():
-	def predicate(ctx):
-		author=ctx.message.author
-		if author is ctx.message.server.owner:
-			return True
-		if any(role.name in approved_roles for role in author.roles):
-			return True
-	return commands.check(predicate)
+    def predicate(ctx):
+        author = ctx.message.author
+        if author is ctx.message.server.owner:
+            return True
+        if any(role.name in approved_roles for role in author.roles):
+            return True
+    return commands.check(predicate)
+
 
 @bot.event
 async def on_ready():
-	print(bot.user.name)
-	print(bot.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
 
-@bot.command(pass_context=True)
-async def add(ctx):
-	''': Add yourself to the queue'''
-	author=ctx.message.author
-	if author not in _queue:
-		_queue.append(author)
-		await bot.send_message(author,'You have been added to the queue')
 
-@bot.command(pass_context=True)
-async def remove(ctx):
-	''': Remove yourself from the queue'''
-	author=ctx.message.author
-	if author in _queue:
-		_queue.remove(author)
-		await bot.send_message(author,'You have been removed from the queue')
-@bot.command()
-async def queue():
-	''': See who's in the queue'''
-	place=1
-	message=''
-	for member in _queue:
-		message+='**#{}** : {}\n'.format(place,member.mention)
-		place+=1
-	if message != '':
-		await bot.say(message)
-	else:
-		await bot.say('Queue is empty')
+class Queue:
 
-@bot.command(pass_context=True)
-async def position(ctx):
-	''': Check your postion in the queue'''
-	author=ctx.message.author
-	if author in _queue:
-		await bot.send_message(author,'You are **#{}** in the queue.'.format(_queue.index(author)+1))
+    def __init__(self, bot):
+        self.bot = bot
+        self.queue = []
+        self.qtoggle = False
 
-@is_approved()
-@bot.command(pass_context=True)
-async def call(ctx):
-	''': Call the next member in the queue'''
-	if len(_queue)>0:
-		member=discord.utils.get(ctx.message.server.members,id=_queue[0])
-		await bot.send_message(member,"You're up, lets play!")
-		await bot.send_message(ctx.message.author,'{} has been sent a message to play'.format(member.name))
-		await bot.say('**{}** has been called up. The queue has been updated.'.format(member.mention))
-		_queue.remove(_queue[0])
+    @commands.command(pass_context=True)
+    async def add(self, ctx):
+        ''': Add yourself to the queue!'''
+        author = ctx.message.author
+        if self.qtoggle:
+            if author.id not in self.queue:
+                self.queue.append(author.id)
+                await self.bot.reply('you have been added to the queue.')
+            else:
+                await self.bot.reply('you are already in the queue!')
+        else:
+            await self.bot.say('The queue is closed.')
 
-@is_approved()
-@bot.command()
-async def clear():
-	''': Clears the queue'''
-	global _queue
-	_queue=[]
-	await bot.say('Queue has been cleared')
+    @commands.command(pass_context=True)
+    async def remove(self, ctx):
+        ''': Remove yourself from the queue'''
+        author = ctx.message.author
+        if author.id in self.queue:
+            self.queue.remove(author.id)
+            await self.bot.reply('you have been removed from the queue.')
+        else:
+            await bot.reply('you were not in the queue.')
+
+    @commands.command(name='queue', pass_context=True)
+    async def _queue(self, ctx):
+        ''': See who's up next!'''
+        server = ctx.message.server
+        message = ''
+        for place, member_id in enumerate(self.queue):
+            member = discord.utils.get(server.members, id=member_id)
+            message += f'**#{place+1}** : {member.mention}\n'
+        if message != '':
+            await self.bot.say(message)
+        else:
+            await self.bot.say('Queue is empty')
+
+    @commands.command(pass_context=True)
+    async def position(self, ctx):
+        ''': Check your position in the queue'''
+        author = ctx.message.author
+        if author.id in self.queue:
+            _position = self.queue.index(author.id)+1
+            await self.bot.reply(f'you are **#{_position}** in the queue.')
+        else:
+            await self.bot.reply(f'you are not in the queue, please use {prefix}add to add yourself to the queue.')
+
+    @is_approved()
+    @commands.command(pass_context=True, name='next')
+    async def _next(self, ctx):
+        ''': Call the next member in the queue'''
+        if len(self.queue) > 0:
+            member = discord.utils.get(
+                ctx.message.server.members, id=self.queue[0])
+            await bot.say(f'You are up **{member.mention}**! Have fun!')
+            self.queue.remove(self.queue[0])
+
+    @is_approved()
+    @commands.command()
+    async def clear(self):
+        ''': Clears the queue'''
+        self.queue = []
+        await self.bot.say('Queue has been cleared')
+
+    @is_approved()
+    @commands.command()
+    async def toggle(self):
+        ''': Toggles the queue'''
+        self.qtoggle = not self.qtoggle
+        if self.qtoggle:
+            state = 'OPEN'
+        else:
+            state = 'CLOSED'
+        await self.bot.say(f'Queue is now {state}')
+
+
+bot.add_cog(Queue(bot))
 
 bot.run('TOKEN')
