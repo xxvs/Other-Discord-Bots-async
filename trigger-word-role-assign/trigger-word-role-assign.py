4bd7b986d9b1c7103e44ad184c96d7d426f90136
stroupbslayen <strouprd@gmail.com>
2018-05-19 08:14:55 -0700

    Added trigger-word-role-assign

---
+++ trigger-word-role-assign/trigger-word-role-assign.py
@@ -0,0 +1,45 @@
+''' Assign a role to a member when they say a word and send a public message telling the server about it'''
+
+import discord
+from discord.ext import commands
+
+# list of words to check for
+trigger_words = ['list', 'of', 'Words']
+
+# The ID# of the role to assign
+assigned_role_id = 'YOUR ROLE ID#'
+
+# bool, change to true if you want the filter to work for case sensitive
+case_sensitive = True
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
+async def on_ready():
+    print(bot.user.name)
+    print(bot.user.id)
+
+
+async def alert(message, role):
+    await bot.add_roles(message.author, role)
+    await bot.send_message(message.channel, f'{message.server.default_role}, {message.author.mention} has been added to {role.mention}!!')
+
+
+@bot.event
+async def on_message(message):
+    assigned_role = discord.utils.get(
+        message.server.roles, id=assigned_role_id)
+    if assigned_role not in message.author.roles:
+        for word in trigger_words:
+            if case_sensitive:
+                if word in message.content:
+                    await alert(message, assigned_role)
+                    return
+            else:
+                if word.lower() in message.content.lower():
+                    await alert(message, assigned_role)
+                    return
+
+
+bot.run('TOKEN')
